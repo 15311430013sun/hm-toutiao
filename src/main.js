bf4a9ccf4b6dcd1c17f555fb18dbd1d895c3d847
‘15311430013sun <1771276489@qq.com>
2019-07-09 16:03:59 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//导入ui包
+import ElementUI from 'element-ui'
+//引入样式 
+import 'element-ui/lib/theme-chalk/index.css'
+//引入路由
+import router from '@/router'
+//注册组件
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
+  //配置路由
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+//导入路由
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+//注册路由
+Vue.use(VueRouter)
+//配置路由对象
+const router = new VueRouter({
+    routes:[
+        {}
+    ]
+})
+//导出路由
+export default router
